39bda101248ad0631076a54b0af4d4e2495a4769
instant-coffee <lachlanrm@gmail.com>
2015-03-18 01:07:40 +0000

    Initial commit

---
+++ exercises/exercise_4.rb
@@ -0,0 +1,11 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+
+puts "Exercise 4"
+puts "----------"
+
+# Your code goes here ...
+
+# Store.where("womans_apparel = ? AND annual_revenue < ?", true, 1000000)
---
+++ exercises/exercise_6.rb
@@ -0,0 +1,17 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+
+puts "Exercise 6"
+puts "----------"
+
+# Your code goes here ...
+
+# @employees_store = Employee.where(first_name: 'Lachlan').first.store
+# Employee.find(1)
+
+# @store.create(...) => @store.new(...) { new creates object in memory} + @store.save
+# new + save
